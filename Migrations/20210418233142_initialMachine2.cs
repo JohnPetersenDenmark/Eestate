2aebe641024184d1ac40d545be7f6c9b1252ac08
JohnPetersenDenmark <55278647+JohnPetersenDenmark@users.noreply.github.com>
2022-02-24 21:58:00 +0100

    ok

---
+++ Migrations/20210418233142_initialMachine2.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Eestate.Migrations
+{
+    public partial class initialMachine2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
